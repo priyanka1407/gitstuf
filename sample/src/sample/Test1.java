bb61d6c12207bf62858c3cf0df56f1df6ebfbc94
priyanka <priyankamurugavel@gmail.com>
2021-03-01 11:56:35 +0530

    first commit

---
+++ sample/src/sample/Test1.java
@@ -0,0 +1,20 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void a() {
+		System.out.println("Test1 a");	
+	}
+	
+	@Test
+	public void b() {
+		System.out.println("Test1 b");	
+	}
+		// TODO Auto-generated method stub
+
+
+
+}
---
+++ sample/src/sample/Test2.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void c() {
+		System.out.println("Test2 c");	
+	}
+	
+	@Test
+	public void d() {
+		System.out.println("Test2 d");	
+	}
+}
